7968f904086b48efe149bd4ad8d2cea181e4b40c
Arthur <21159051+ArthurVardevanyan@users.noreply.github.com>
2020-03-20 17:45:52 -0400

    authorization

--- spotify.py
+++ spotify.py
@@ -2,21 +2,16 @@ __version__ = "v20200320"
 
 
 import requests
-
-key = ""
-try:
-    with open("Credentials/spotifyToken.txt") as f:
-        key = f.readlines()
-    key = [x.strip() for x in key]
-    key = str(key[0])
-except:
-    print("Credential Failure")
+from authorization import authorization as authorize
+import time
 
 url = 'https://api.spotify.com/v1/me/player/currently-playing?market=ES'
 header = {"Accept": "application/json",
-          "Content-Type": "application/json", "Authorization": "Bearer " + key}
+          "Content-Type": "application/json", "Authorization": "Bearer " + authorize()}
 
-response = requests.get(url, headers=header)
-response = response.json()
-track = response.get("item").get("name")
-print(track)
+while(True):
+    response = requests.get(url, headers=header)
+    response = response.json()
+    track = response.get("item").get("name")
+    print(track)
+    time.sleep(1)
