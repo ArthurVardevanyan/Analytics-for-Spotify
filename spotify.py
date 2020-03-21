25161cdaab87b05e7dc40c03a94d880d9ec660d0
Arthur <arthur@arthurvardevanyan.com>
2020-03-20 20:20:12 -0400

    database

--- spotify.py
+++ spotify.py
@@ -4,14 +4,22 @@ __version__ = "v20200320"
 import requests
 from authorization import authorization as authorize
 import time
+import database
 
 url = 'https://api.spotify.com/v1/me/player/currently-playing?market=ES'
 header = {"Accept": "application/json",
           "Content-Type": "application/json", "Authorization": "Bearer " + authorize()}
-
+previous =" "
 while(True):
+    
     response = requests.get(url, headers=header)
     response = response.json()
     track = response.get("item").get("name")
-    print(track)
+    if(not response.get("is_local")):
+        if(previous != track):
+            if(int(response.get("progress_ms")) > 30000):
+                print(track)
+                previous = track
+                print("Song Counted as Played")   
+                database.database_input(response)  
     time.sleep(1)
