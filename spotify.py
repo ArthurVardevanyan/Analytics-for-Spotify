99ffdb52af090598bbc3a5c44e689b3015a7ecd9
Arthur <arthur@arthurvardevanyan.com>
2020-03-21 09:44:51 -0400

    No Song Playing

--- spotify.py
+++ spotify.py
@@ -1,4 +1,4 @@
-__version__ = "v20200320"
+__version__ = "v20200321"
 
 
 import requests
@@ -9,23 +9,27 @@ import database
 url = 'https://api.spotify.com/v1/me/player/currently-playing?market=ES'
 header = {"Accept": "application/json",
           "Content-Type": "application/json", "Authorization": "Bearer " + authorize()}
-previous =" "
+previous = " "
 while(True):
     response = requests.get(url, headers=header)
     if("the access token expired" in str.lower(response.text)):
         header = {"Accept": "application/json",
-          "Content-Type": "application/json", "Authorization": "Bearer " + authorize()}
+                  "Content-Type": "application/json", "Authorization": "Bearer " + authorize()}
         response = requests.get(url, headers=header)
-    response = response.json()
-    track = response.get("item").get("name")
-    if(not response.get("item").get("is_local")):
-        if(previous != track):
-            if(int(response.get("progress_ms")) > 30000):
-                print(track)
-                previous = track
-                print("Song Counted as Played")   
-                database.database_input(response)  
-                time.sleep(25)
+    elif("no content" not in str.lower(response.reason)):
+        response = response.json()
+        track = response.get("item").get("name")
+        if(not response.get("item").get("is_local")):
+            if(previous != track):
+                if(int(response.get("progress_ms")) > 30000):
+                    print(track)
+                    previous = track
+                    print("Song Counted as Played")
+                    database.database_input(response)
+                    time.sleep(25)
+        else:
+            time.sleep(60)
     else:
+        print("Nothing is Playing")
         time.sleep(60)
     time.sleep(1)
