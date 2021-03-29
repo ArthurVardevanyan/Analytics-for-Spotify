6aef6d1de99ba2db0f70c685a85dea0de12e4d5a
Arthur <arthur@arthurvardevanyan.com>
2021-03-29 18:20:17 -0400

    Extra File Removal

--- AnalyticsforSpotify/settings.py
+++ AnalyticsforSpotify/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = HOSTS
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
