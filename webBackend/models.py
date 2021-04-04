b17616f7a2bd57b9fdfb903c50bb08bcce8b21e3
Arthur <arthur@arthurvardevanyan.com>
2021-04-04 10:34:32 -0400

    Formatted

--- webBackend/models.py
+++ webBackend/models.py
@@ -20,8 +20,10 @@ class Artists(models.Model):
 class Listeninghistory(models.Model):
     user = models.ForeignKey('Users', models.DO_NOTHING, db_column='user')
     timestamp = models.BigIntegerField(primary_key=True)
-    timeplayed = models.TextField(db_column='timePlayed')  # Field name made lowercase.
-    songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='songID')  # Field name made lowercase.
+    # Field name made lowercase.
+    timeplayed = models.TextField(db_column='timePlayed')
+    # Field name made lowercase.
+    songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='songID')
     json = models.TextField(blank=True, null=True)
 
     class Meta:
@@ -31,9 +33,12 @@ class Listeninghistory(models.Model):
 
 
 class Playcount(models.Model):
-    user = models.OneToOneField('Users', models.DO_NOTHING, db_column='user', primary_key=True)
-    songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='songID')  # Field name made lowercase.
-    playcount = models.IntegerField(db_column='playCount')  # Field name made lowercase.
+    user = models.OneToOneField(
+        'Users', models.DO_NOTHING, db_column='user', primary_key=True)
+    # Field name made lowercase.
+    songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='songID')
+    # Field name made lowercase.
+    playcount = models.IntegerField(db_column='playCount')
 
     class Meta:
         managed = False
@@ -42,9 +47,13 @@ class Playcount(models.Model):
 
 
 class Playlistsongs(models.Model):
-    playlistid = models.OneToOneField('Playlists', models.DO_NOTHING, db_column='playlistID', primary_key=True)  # Field name made lowercase.
-    songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='songID')  # Field name made lowercase.
-    songstatus = models.TextField(db_column='songStatus')  # Field name made lowercase.
+    # Field name made lowercase.
+    playlistid = models.OneToOneField(
+        'Playlists', models.DO_NOTHING, db_column='playlistID', primary_key=True)
+    # Field name made lowercase.
+    songid = models.ForeignKey('Songs', models.DO_NOTHING, db_column='songID')
+    # Field name made lowercase.
+    songstatus = models.TextField(db_column='songStatus')
 
     class Meta:
         managed = False
@@ -56,7 +65,8 @@ class Playlists(models.Model):
     user = models.ForeignKey('Users', models.DO_NOTHING, db_column='user')
     id = models.CharField(primary_key=True, max_length=128)
     name = models.CharField(max_length=128)
-    lastupdated = models.TextField(db_column='lastUpdated')  # Field name made lowercase.
+    # Field name made lowercase.
+    lastupdated = models.TextField(db_column='lastUpdated')
 
     class Meta:
         managed = False
@@ -65,8 +75,12 @@ class Playlists(models.Model):
 
 
 class Songartists(models.Model):
-    songid = models.OneToOneField('Songs', models.DO_NOTHING, db_column='songID', primary_key=True)  # Field name made lowercase.
-    artistid = models.ForeignKey(Artists, models.DO_NOTHING, db_column='artistID')  # Field name made lowercase.
+    # Field name made lowercase.
+    songid = models.OneToOneField(
+        'Songs', models.DO_NOTHING, db_column='songID', primary_key=True)
+    # Field name made lowercase.
+    artistid = models.ForeignKey(
+        Artists, models.DO_NOTHING, db_column='artistID')
 
     class Meta:
         managed = False
@@ -77,7 +91,8 @@ class Songartists(models.Model):
 class Songs(models.Model):
     id = models.CharField(primary_key=True, max_length=22)
     name = models.TextField()
-    tracklength = models.BigIntegerField(db_column='trackLength')  # Field name made lowercase.
+    # Field name made lowercase.
+    tracklength = models.BigIntegerField(db_column='trackLength')
 
     class Meta:
         managed = False
@@ -95,10 +110,13 @@ class Spotifyapi(models.Model):
 class Users(models.Model):
     user = models.CharField(primary_key=True, max_length=128)
     enabled = models.IntegerField()
-    statussong = models.IntegerField(db_column='statusSong')  # Field name made lowercase.
-    statusplaylist = models.IntegerField(db_column='statusPlaylist')  # Field name made lowercase.
+    # Field name made lowercase.
+    statussong = models.IntegerField(db_column='statusSong')
+    # Field name made lowercase.
+    statusplaylist = models.IntegerField(db_column='statusPlaylist')
     cache = models.TextField()
-    realtime = models.IntegerField(db_column='realTime')  # Field name made lowercase.
+    # Field name made lowercase.
+    realtime = models.IntegerField(db_column='realTime')
 
     class Meta:
         managed = False
