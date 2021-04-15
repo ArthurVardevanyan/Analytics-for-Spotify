fe69d7dcd5e44f1898064ee3b9f257a40c74eeda
Arthur <arthur@arthurvardevanyan.com>
2021-04-14 20:16:54 -0400

    test

--- webBackend/tests.py
+++ webBackend/tests.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
 import webBackend.views as views
 
+
+class testObject(object):
+    pass
+
+
 class TestCase(TestCase):
-    def test(self):
+    def test_boot(self):
         views.boot()
+        self.assertEqual(1, 1)
+
+    def test_authenticated(self):
+        req = testObject()
+        req.session = {}
+        views.authenticated(req)
         self.assertEqual(1, 1)
