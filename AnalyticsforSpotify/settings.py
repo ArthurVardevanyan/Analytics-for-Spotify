1791bfff30e374bb760692bde7d3e4a8b8bfc45c
Arthur <arthur@arthurvardevanyan.com>
2021-11-03 23:14:37 -0400

    CSRF: Cors Origin

--- AnalyticsforSpotify/settings.py
+++ AnalyticsforSpotify/settings.py
@@ -39,11 +39,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # Local Only Acesss 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',  # Comment for Testing
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False  # True For Testing
 
 ROOT_URLCONF = 'AnalyticsforSpotify.urls'
 
