a636494d1cdfb7d18aa5367cc848cebf5237ce34
Arthur <arthur@arthurvardevanyan.com>
2022-10-29 12:36:10 -0400

    Setup.py Code Documentation

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 import base64
-import binascii
 import urllib.parse
 import json
 import tarfile
@@ -8,11 +7,37 @@ import MySQLdb
 
 
 def extractNode():
+    """
+    Extracts Node Modules, only used when running outside of a container.
+
+    Parameters:
+        None
+    Returns:
+        bool: unused return
+    """
     with tarfile.open("webFrontend/node_modules.tar.xz", 'r') as tar:
         tar.extractall("webFrontend/")
 
+    return 0
+
 
 def containerSetup():
+    """
+    Pull Setup Variables from Environment,
+    and setup setup container runtime configs.
+
+     Parameters:
+        CLIENT  (str)   : Spotify Client ID
+        SECRET  (str)   : Spotify Client Secret
+        R_URL   (str)   : Spotify Redirect URL
+
+        IP      (str)   : Database IP / Hostname
+        DB      (str)   : Database Name
+        USER    (str)   : Database Username
+        PASS    (str)   : Database Password
+    Returns:
+        bool: unused return
+    """
     CLIENT = os.environ.get('CLIENT_ID')
     SECRET = os.environ.get('CLIENT_SECRET')
     R_URL = os.environ.get('REDIRECT_URL')
@@ -26,19 +51,23 @@ def containerSetup():
     db = myCNF(IP, DB, USER, PASS)
     setup(db, API)
 
-    # Check if thier are users in the database, mark as disabled, run manage, then re-enable.
+    # TODO: Check if their are users in the database, mark as disabled, run manage, then re-enable.
+
+    return 0
 
 
-def myCNF(IP, DB, USER, PASS):
-    MYSQL = [
-        "[client]",
-        "host = " + IP,
-        "database = " + DB,
-        "user = "+USER,
-        "password = "+PASS,
-        "default-character-set = utf8",
-    ]
+def myCNF(IP: str, DB: str, USER: str, PASS: str):
+    """
+    Creates an MySQL Connection Object
 
+    Parameters:
+        IP      (str)   : Database IP / Hostname
+        DB      (str)   : Database Name
+        USER    (str)   : Database Username
+        PASS    (str)   : Database Password
+    Returns:
+        MySQLdb: MySQL Connection Object
+    """
     db = MySQLdb.connect(
         host=IP,
         user=USER,
@@ -49,7 +78,17 @@ def myCNF(IP, DB, USER, PASS):
     return db
 
 
-def spotifyAPI(CLIENT, SECRET, R_URL):
+def spotifyAPI(CLIENT: str, SECRET: str, R_URL: str):
+    """
+    Setup Spotify Configuration for Injection into to DB
+
+     Parameters:
+        CLIENT  (str)   : Spotify Client ID
+        SECRET  (str)   : Spotify Client Secret
+        R_URL   (str)   : Spotify Redirect URL
+    Returns:
+        dict: Dictionary Containing Spotify API Configuration
+    """
     B64CS = str(base64.b64encode(
         ":".join([CLIENT, SECRET]).encode("utf-8")), "utf-8")
     SCOPES = "&scope=user-read-currently-playing+user-read-recently-played"
@@ -66,7 +105,18 @@ def spotifyAPI(CLIENT, SECRET, R_URL):
     return API
 
 
-def setup(db, API):
+def setup(db: MySQLdb.connections, API: dict):
+    """
+    If new Instance, sets up the database, and injects API Config.
+    If existing Instance, updates the API Config.
+
+    Parameters:
+        db   (MySQLdb): Database Connection Object
+        API  (dict)   : Spotify API Configuration
+
+    Returns:
+        bool: unused return
+    """
     cursor = db.cursor()
     users = 'show tables like "users"'
     cursor.execute(users)
@@ -89,8 +139,30 @@ def setup(db, API):
     db.commit()
     db.close
 
+    return 0
+
 
 def main():
+    """
+    Init Setup Script before loading Application,
+    setups database initial database,
+    and injects Spotify API Credentials
+
+    Supports Manually Inputting Parameters,
+    or pulling from the environment.
+
+     Parameters:
+        CLIENT  (str)   : Spotify Client ID
+        SECRET  (str)   : Spotify Client Secret
+        R_URL   (str)   : Spotify Redirect URL
+
+        IP      (str)   : Database IP / Hostname
+        DB      (str)   : Database Name
+        USER    (str)   : Database Username
+        PASS    (str)   : Database Password
+    Returns:
+        bool: Script Exit Condition
+    """
     print("*Disclaimer*, DO NOT USE WITH PUBLIC ACCESS")
     if(os.environ.get('DOCKER')):
         containerSetup()
@@ -108,6 +180,8 @@ def main():
         PASS = input("Enter Password:")
         db = myCNF(IP, DB, USER, PASS)
         setup(db, API)
+
+    return 0
 
 
 if __name__ == "__main__":
